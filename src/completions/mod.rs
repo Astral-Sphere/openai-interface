3c7cff648c3eb33fdb27abaeb3b912963533d5e0
hammerklavier <q5vsx3@163.com>
2025-09-22 22:24:34 +0800

    Add deprecation notice for completions API
    The completions API is being deprecated in favor of the chat API, which
    supports multiple rounds of conversation.

--- src/completions/mod.rs
+++ src/completions/mod.rs
@@ -1,4 +1,7 @@
 //! Given a prompt, the model will return one or more predicted completions,
 //! and can also return the probabilities of alternative tokens at each position.
+//! Compared to the `chat` API, this one does not provide the ability to have
+//! multiple rounds of conversation. This API is getting deprecated in favor of the
+//! `chat` API.
 
 pub mod request;
