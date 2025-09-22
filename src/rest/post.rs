ff84f52023d2ae70541a00755960afecb6ff2512
hammerklavier <q5vsx3@163.com>
2025-09-22 17:43:40 +0800

    Refactor NoStream trait to use async fn in trait
    This commit updates the NoStream trait to use the new async fn in trait
    feature. The get_response method now returns an impl Future instead of
    being declared as async, which allows the trait to be object-safe while
    maintaining the same async behavior. The implementation is wrapped in an
    async block and the trait now requires Sync + Send bounds.

--- src/rest/post.rs
+++ src/rest/post.rs
@@ -1,35 +1,45 @@
+use std::future::Future;
+
 use serde::Serialize;
 
 use crate::errors::RequestError;
 
-#[allow(dead_code)]
-pub(crate) trait NoStream: Serialize {
-    async fn get_response(&self, url: &str, key: &str) -> Result<String, RequestError> {
-        let client = reqwest::Client::new();
-        let response = client
-            .post(url)
-            .headers({
-                let mut headers = reqwest::header::HeaderMap::new();
-                headers.insert("Content-Type", "application/json".parse().unwrap());
-                headers.insert("Accept", "application/json".parse().unwrap());
-                headers
-            })
-            .bearer_auth(key)
-            .json(self)
-            .send()
-            .await
-            .map_err(|e| RequestError::SendError(format!("Failed to send request: {:#?}", e)))?;
+pub trait NoStream: Serialize + Sync + Send {
+    fn get_response(
+        &self,
+        url: &str,
+        key: &str,
+    ) -> impl Future<Output = Result<String, RequestError>> + Send + Sync {
+        async move {
+            let client = reqwest::Client::new();
+            let response = client
+                .post(url)
+                .headers({
+                    let mut headers = reqwest::header::HeaderMap::new();
+                    headers.insert("Content-Type", "application/json".parse().unwrap());
+                    headers.insert("Accept", "application/json".parse().unwrap());
+                    headers
+                })
+                .bearer_auth(key)
+                .json(self)
+                .send()
+                .await
+                .map_err(|e| {
+                    RequestError::SendError(format!("Failed to send request: {:#?}", e))
+                })?;
 
-        if response.status() != reqwest::StatusCode::OK {
-            return Err(
-                crate::errors::RequestError::ResponseStatus(response.status().as_u16()).into(),
-            );
+            if response.status() != reqwest::StatusCode::OK {
+                return Err(crate::errors::RequestError::ResponseStatus(
+                    response.status().as_u16(),
+                )
+                .into());
+            }
+
+            let text = response.text().await.map_err(|e| {
+                RequestError::ResponseError(format!("Failed to get response text: {:#?}", e))
+            })?;
+
+            Ok(text)
         }
-
-        let text = response.text().await.map_err(|e| {
-            RequestError::ResponseError(format!("Failed to get response text: {:#?}", e))
-        })?;
-
-        Ok(text)
     }
 }
